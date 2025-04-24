82e27ed1a7c63bb162fa0a7dafd8fe3ba3813c6d
Srv108 <srv.br009@gmail.com>
2025-04-24 20:32:40 +0530

    Initial Commit

---
+++ FitnessAppTests/FitnessAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppTests.swift
+//  FitnessAppTests
+//
+//  Created by Saurabh Kumar on 24/04/25.
+//
+
+import Testing
+@testable import FitnessApp
+
+struct FitnessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
